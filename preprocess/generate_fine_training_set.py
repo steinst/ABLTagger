09a4ed25f33d0c9ab218aafe3bffd4ad43b0eb33
steinst <steinikallinn@gmail.com>
2020-04-01 01:37:54 +0000

    - added script to generate fine grained

---
+++ preprocess/generate_fine_training_set.py
@@ -0,0 +1,29 @@
+import sys
+import argparse
+
+def finify(inputline):
+    try:
+        temp = inputline.strip().split()
+        return temp[0] + '\t' + temp[1] + '\n'
+    except:
+        return '\n'
+
+if __name__ == '__main__':
+    # reading input parameters
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--input', '-i', help='Name of input file.', default="Full.txt")
+    parser.add_argument('--output', '-o', help='Name of output file.', default="Full.fine.txt")
+
+    try:
+        args = parser.parse_args()
+    except:
+        sys.exit(0)
+
+    with open(args.output, "w") as f:
+        with open(args.input) as training:
+            line = training.readline()
+            while line:
+                f.write(finify(line))
+                line = training.readline()
+
+#TODO: add spinner and something to indicate time left
